a59efe215d33d1e16b55044b6d27860b03fa8043
Miaan <nghiabn2005@gmail.com>
2024-02-14 22:31:47 +0700

    re do movement system

---
+++ Assets/Source/Player/PlayerMotionManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotionManager : MonoBehaviour
+{
+    public PlayerManager manager { get; set; }
+
+    private const float RATIO = 50f;
+
+    private float _currentVelocity;
+    public float currentVelocityMagnitude { get => _currentVelocity; }
+
+    public void DelegateStart()
+    {
+
+    }
+
+    public void DelegateUpdate()
+    {
+
+    }
+
+    public void DelegateFixedUpdate()
+    {
+        _currentVelocity = manager.body.velocity.magnitude;
+    }
+
+    public void AddBonusVelocity(Vector2 velocityAdd)
+    {
+        manager.body.AddForce(velocityAdd * RATIO);
+    }
+}
